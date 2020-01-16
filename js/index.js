64a69327795f5e06704d7b0bc9676b03adb65705
kys <tlfldntm12@naver.com>
2020-01-16 23:53:36 +0900

    깃 테스트

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-console.log('load');
+
+
+function list() {
+
+    const $listWrap = document.getElementById("listWrap");
+    const $listItem = document.getElementsByClassName("listItem");
+
+    // button
+    const $deleteBut = document.getElementsByClassName("delete_but");
+
+    console.log($deleteBut.target);
+    $deleteBut.addEventListener("click", function () {
+        console.log('1')
+    });
+
+}
+list()
